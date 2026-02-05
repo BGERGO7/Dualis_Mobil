8efe0b9f014cde9b9a91f18bf3d52b8030905b69
Bükki Gergely <colt-reopen-tree@duck.com>
2026-02-05 11:00:26 +0100

    2026.02.05 finish

---
+++ mobil-backend/README.md
@@ -0,0 +1,13 @@
+## Futtatás
+
+```bash
+# Függőségek telepítése
+npm install
+
+# Csomagook telepítése (expo)
+npx expo install {name}
+
+# Projekt inditasa
+npm run start
+
+```
--- mobil-backend/controllers/apiController.js
+++ mobil-backend/controllers/apiController.js
@@ -1,101 +1,105 @@
-let Models = require('../models/index');
-let axios = require('axios');
+let Models = require("../models/index");
+let axios = require("axios");
 
 require("dotenv").config();
 
-let addArticle = (title, content, contentLong,  authorName, imageUrl) => {
-    return Models.Post.create({
-        title: title,
-        content: content,
-        contentLong: contentLong,
-        authorName: authorName,
-        imageUrl: imageUrl,
-    });
-}
+let addArticle = (title, content, contentLong, authorName, imageUrl) => {
+  return Models.Post.create({
+    title: title,
+    content: content,
+    contentLong: contentLong,
+    authorName: authorName,
+    imageUrl: imageUrl,
+  });
+};
 
 exports.registerArticle = (req, res) => {
-    console.log("Hozzáadok egy új cikket...");
+  console.log("Hozzáadok egy új cikket...");
 
-    const { title, content, contentLong,  authorName, imageUrl } = req.body;
+  const { title, content, contentLong, authorName, imageUrl } = req.body;
 
-    addArticle(title, content, contentLong,  authorName, imageUrl)
+  addArticle(title, content, contentLong, authorName, imageUrl)
     .then((result) => {
-        res.status(201).send("Az adat létrejött.");
+      res.status(201).send("Az adat létrejött.");
     })
     .catch((err) => {
-        res.status(500).send("Hiba történt az adat létrehozásakor.");
+      res.status(500).send("Hiba történt az adat létrehozásakor.");
     });
 };
 
 exports.getAllArticles = (req, res) => {
-    Models.Post.findAll({
-        order: [['title', 'ASC']]
-    })
+  Models.Post.findAll({
+    order: [["title", "ASC"]],
+  })
     .then((result) => {
-        if(!result){
-            return res.status(404).json({
-                error: "Jelenleg nincs bejegyzés az adatbázisban."
-            });
-        }
-        res.status(200).json({data: result});
+      if (!result) {
+        return res.status(404).json({
+          error: "Jelenleg nincs bejegyzés az adatbázisban.",
+        });
+      }
+      res.status(200).json({ data: result });
     })
     .catch((err) => {
-        res.status(500).json({error: "Hiba történt az adatok lekérdezésekor."});
+      res.status(500).json({ error: "Hiba történt az adatok lekérdezésekor." });
     });
 };
 
 exports.getArticle = (req, res) => {
-    const {id} = req.params;
+  const { id } = req.params;
 
-    Models.Post.findOne({
-        where: {id: id}
-    })
+  Models.Post.findOne({
+    where: { id: id },
+  })
     .then((result) => {
-        if(!result){
-            return res.status(404).json({
-                error: `Nincs ${id} azonosítóval bejegyzés az adatbázisban.`
-            });
-        }
-        res.status(200).json({data: result});
+      if (!result) {
+        return res.status(404).json({
+          error: `Nincs ${id} azonosítóval bejegyzés az adatbázisban.`,
+        });
+      }
+      res.status(200).json({ data: result });
     })
     .catch((err) => {
-        res.status(500).json({error: "Hiba történt az adat lekérdezésekor."});
+      res.status(500).json({ error: "Hiba történt az adat lekérdezésekor." });
     });
 };
 
 exports.deleteArticle = (req, res) => {
-    const {id} = req.params;
+  const { id } = req.params;
 
-    Models.Post.destroy({
-        where: {id: id}
-    })
-    .then(() => {
-        res.status(418).json({message: `A ${id} azonosítójú bejegyzés törölve lett.`});
-    });
+  Models.Post.destroy({
+    where: { id: id },
+  }).then(() => {
+    res
+      .status(418)
+      .json({ message: `A ${id} azonosítójú bejegyzés törölve lett.` });
+  });
 };
 
 exports.getPokemon = async (req, res) => {
-    const name = req.params.name.toLowerCase();
+  const name = req.params.name.toLowerCase();
 
-    try{
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-        const pokeData = response.data;
+  try {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${name}`,
+    );
+    const pokeData = response.data;
 
-        const customPokemon = {
-            id : pokeData.id,
-            name : pokeData.name,
-            height : pokeData.height,
-            weight : pokeData.weight,
-            image: pokeData.sprites.other["official-artwork"].front_default,
-            type : pokeData.types[0].type,
-        };
-
-        res.status(200).json({data: customPokemon});
-    } 
-    catch (error) {
-        if(error.response && error.response.status === 404){
-            return res.status(404).json({error: `gatya`});
-        }
-        res.status(500).json({error: "Hiba történt a Pokémon adatainak lekérésekor."});
+    const customPokemon = {
+      id: pokeData.id,
+      name: pokeData.name,
+      height: pokeData.height,
+      weight: pokeData.weight,
+      image: pokeData.sprites.other["official-artwork"].front_default,
+      type: pokeData.types[0].type.name,
     };
+
+    res.status(200).json({ customPokemon });
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      return res.status(404).json({ error: `gatya` });
+    }
+    res
+      .status(500)
+      .json({ error: "Hiba történt a Pokémon adatainak lekérésekor." });
+  }
 };
